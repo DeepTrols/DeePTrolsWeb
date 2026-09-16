6dc2d12db96548ad2313bce24167e52f6fa382c5
DeepTrols <quanlei@deeptrols.com>
2026-09-16 20:56:28 +0800

    fix(TASK-014.28): align dgp architecture flow width

--- components/product/dgp/DgpArchitecture.vue
+++ components/product/dgp/DgpArchitecture.vue
@@ -14,8 +14,8 @@ import { dgpSystemCards } from '~/data/dgp'
     label="数曜·数据治理平台产品架构图占位"
     fallback-text="产品架构图占位符"
   >
-    <div class="relative z-[1] h-full w-full overflow-hidden">
-      <div class="absolute left-1/2 top-1/2 h-[460px] w-[1704px] -translate-x-1/2 -translate-y-1/2 scale-[0.58] xl:scale-[0.72] 2xl:scale-[0.88]">
+    <div class="relative z-[1] mx-auto h-full w-[min(1600px,100%)] overflow-hidden">
+      <div class="absolute left-1/2 top-1/2 h-[460px] w-full -translate-x-1/2 -translate-y-1/2">
         <FlowMqHowItWorksFlow />
       </div>
     </div>
